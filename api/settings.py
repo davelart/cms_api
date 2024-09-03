e93ddb70fcb59e967c972335de94967660378c1c
larteydavid59 <larteydavid59@gmail.com>
2024-09-03 18:25:46 +0000

    database configured

--- api/settings.py
+++ api/settings.py
@@ -174,11 +174,25 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.blqkoylsoxzfnfmvuwaf',
+        'PASSWORD': 'onlyGodcanstopme',
+        'HOST': 'aws-0-us-east-1.pooler.supabase.com',
+        'PORT': 6543,
+        'OPTIONS': {
+            # 'sslmode': 'verify-full'
+        },
+    },
 }
 
 
